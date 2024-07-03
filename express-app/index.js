3811bac1623d1a7481ec67c1580ac1b928503898
tarunkumar <mallangitarun@hotmail.com>
2024-07-03 11:00:35 +0530

    Updated <h1> message in index.js of express app

--- express-app/index.js
+++ express-app/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello World!</h1>')
+  res.send('<h1>Hello World! This is express app.</h1>')
   res.send('<h2>How are you?<h2>')	
 })
 
